ebfa566615545602b17237684b17ca08f0348083
lamngocminh <lamngocminh1003@gmail.com>
2023-04-11 12:46:10 +0700

    create migration

---
+++ server/app/Interviewer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Interviewer extends Model
+{
+    protected $table = 'interviewer'; 
+    protected $fillable = ['id','del_flag','interview_id','member_id','evaluation',
+    'created_by','modified_by','created_at','updated_at']; 
+
+    public function member(){
+        return $this->belongsTo(Member::class, 'member_id', 'id');
+    }
+
+    public function interview(){
+        return $this->belongsTo(Interview::class, 'interview_id', 'id');
+    }
+
+    public function interviewer_comments(){
+        return $this->hasMany(InterviewerComment::class, 'interviewer_id', 'id');
+    }
+}
---
+++ server/app/Member.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    protected $table = 'member';
+    protected $fillable = ['id','del_flag','username','password','fullname','is_male','birthday',
+    'email','phone','picture','access_level','created_by','modified_by','created_at','updated_at'];
+
+    protected $hidden = [
+        'password', 'pivot'
+    ];
+
+    public function teams(){
+        return $this->belongsToMany(Team::class, TeamMember::class, 'member_id', 'team_id');
+    }
+
+    public function team_members(){
+        return $this->hasMany(TeamMember::class, 'member_id', 'id');
+    }
+
+    public function member_role(){
+        return $this->belongsTo(Configuration::class, 'access_level', 'id');
+    }
+
+    public function leader_of_teams(){
+        return $this->hasMany(Team::class, 'leader', 'id');
+    }
+
+    public function interviews(){
+        return $this->belongsToMany(Interview::class, Interviewer::class, 'member_id', 'interview_id');
+    }
+
+    public function interviewers(){
+        return $this->hasMany(Interviewer::class, 'member_id', 'id');
+    }
+
+    public function interviewer_comment(){
+        return $this->hasManyThrough(InterviewerComment::class, Interviewer::class, 'member_id', 'interviewer_id', 'id', 'id');
+    }
+}
